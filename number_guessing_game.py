2973a9ba67b3ff0ad1ea00c2e4a95f658eeeced3
CoderPOOP <95227359+CoderPOOP@users.noreply.github.com>
2022-02-03 21:12:32 +0530

    Add files via upload

---
+++ number_guessing_game.py
@@ -0,0 +1,14 @@
+# Number Guessing Game
+
+secret_number = 9
+guess_count = 0
+guess_limit = 3
+
+while guess_count < guess_limit:
+    guess = int(input('Guess: '))
+    guess_count += 1
+    if guess == secret_number:
+        print("You Won!")
+        break
+    else:
+        print("You Lose!")
